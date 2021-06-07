1f310c9568a22fe3db78812a9e5f440b1a281b86
juliocesardw <67384316+juliocesardw@users.noreply.github.com>
2021-06-07 01:15:38 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,58 @@
+const headerMenu=document.querySelector('.hm-header');
+
+console.log(headerMenu.offsetTop);
+
+window.addEventListener('scroll',()=>{
+    if(window.pageYOffset > 80){
+        headerMenu.classList.add('header-fixed');
+    }else{
+        headerMenu.classList.remove('header-fixed');
+    }
+})
+
+/*=========================================
+    Tabs
+==========================================*/
+if(document.querySelector('.hm-tabs')){
+
+    const tabLinks=document.querySelectorAll('.hm-tab-link');
+    const tabsContent=document.querySelectorAll('.tabs-content');
+
+    tabLinks[0].classList.add('active');
+
+    if(document.querySelector('.tabs-content')){
+        tabsContent[0].classList.add('tab-active');
+    }
+    
+
+    for (let i = 0; i < tabLinks.length; i++) {
+        
+        tabLinks[i].addEventListener('click',()=>{
+
+            
+            tabLinks.forEach((tab) => tab.classList.remove('active'));
+            tabLinks[i].classList.add('active');
+            
+            tabsContent.forEach((tabCont) => tabCont.classList.remove('tab-active'));
+            tabsContent[i].classList.add('tab-active');
+            
+        });
+        
+    }
+
+}
+
+/*=========================================
+    MENU
+==========================================*/
+
+const menu=document.querySelector('.icon-menu');
+const menuClose=document.querySelector('.cerrar-menu');
+
+menu.addEventListener('click',()=>{
+    document.querySelector('.header-menu-movil').classList.add('active');
+})
+
+menuClose.addEventListener('click',()=>{
+    document.querySelector('.header-menu-movil').classList.remove('active');
+})
